3da1b8bdf08c47ec1c16a3efec2206443c4bfee9
rails -v <vanessa.gabriel.tuesta@gmail.com>
2012-07-14 13:52:47 -0500

    primer commit

---
+++ tienda/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tienda::Application.config.secret_token = 'af7c4e2bb158980ed170e6b6e827cee5b66a1cade66139e4cbb1b6b37224bd6829c7120005c7124cd89cc2a9ad0b39bf506e228f770b3450f6553161e6f0037f'
